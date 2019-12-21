0eb7812215caeef44b38b4f0d9eb357e26b2a23b
Billy Yuan <billy112487983@gmail.com>
2019-12-21 20:11:45 +0800

    context testing
    Signed-off-by: Billy Yuan <billy112487983@gmail.com>

--- vertx-pg-client/src/test/java/io/vertx/pgclient/ContextTest.java
+++ vertx-pg-client/src/test/java/io/vertx/pgclient/ContextTest.java
@@ -20,19 +20,35 @@ public class ContextTest extends PgTestBase {
   }
 
   @After
-  public void teardown(TestContext ctx) {
+  public void tearDown(TestContext ctx) {
     vertx.close(ctx.asyncAssertSuccess());
   }
 
   @Test
-  public void testConnection(TestContext testCtx) {
-    Async async = testCtx.async();
-    Context connCtx = vertx.getOrCreateContext();
-    connCtx.runOnContext(v1 -> {
-      PgConnection.connect(vertx, options, testCtx.asyncAssertSuccess(conn -> {
-        testCtx.assertEquals(connCtx, Vertx.currentContext());
-        conn.query("SELECT *  FROM (VALUES ('Hello world')) t1 (col1) WHERE 1 = 1", testCtx.asyncAssertSuccess(result -> {
-          testCtx.assertEquals(connCtx, Vertx.currentContext());
+  public void testConnectionNotInContext(TestContext testContext) {
+    Async async = testContext.async();
+
+    PgConnection.connect(vertx, options, testContext.asyncAssertSuccess(conn -> {
+      Context connContext = Vertx.currentContext();
+      testContext.assertNotNull(connContext);
+      conn.query("SELECT 1", testContext.asyncAssertSuccess(result -> {
+        testContext.assertEquals(connContext, Vertx.currentContext());
+        conn.close();
+        async.complete();
+      }));
+    }));
+  }
+
+  @Test
+  public void testConnectionInSameContext(TestContext testContext) {
+    Async async = testContext.async();
+    Context appCtx = vertx.getOrCreateContext();
+    appCtx.runOnContext(v1 -> {
+      PgConnection.connect(vertx, options, testContext.asyncAssertSuccess(conn -> {
+        testContext.assertEquals(appCtx, Vertx.currentContext());
+        conn.query("SELECT 1", testContext.asyncAssertSuccess(result -> {
+          testContext.assertEquals(appCtx, Vertx.currentContext());
+          conn.close();
           async.complete();
         }));
       }));
@@ -40,21 +56,98 @@ public class ContextTest extends PgTestBase {
   }
 
   @Test
-  public void testPooledConnection(TestContext testCtx) {
+  public void testConnectionInDifferentContext(TestContext testContext) {
+    Async async = testContext.async();
+    Context appCtx1 = vertx.getOrCreateContext();
+    Context appCtx2 = vertx.getOrCreateContext();
+    testContext.assertNotEquals(appCtx1, appCtx2);
+    appCtx1.runOnContext(v1 -> {
+      PgConnection.connect(vertx, options, testContext.asyncAssertSuccess(conn -> {
+        testContext.assertEquals(appCtx1, Vertx.currentContext());
+        appCtx2.runOnContext(v2 -> {
+          testContext.assertEquals(appCtx2, Vertx.currentContext());
+          conn.query("SELECT 1", testContext.asyncAssertSuccess(result -> {
+            testContext.assertEquals(appCtx1, Vertx.currentContext()); //FIXME appCtx1?
+            conn.close();
+            async.complete();
+          }));
+        });
+      }));
+    });
+  }
+
+  @Test
+  public void testPooledConnectionNotInContext(TestContext testContext) {
     Context appCtx = vertx.getOrCreateContext();
-    Async async = testCtx.async();
+    Async async = testContext.async();
+    PgPool pool = PgPool.pool(vertx, options, new PoolOptions());
+    pool.getConnection(testContext.asyncAssertSuccess(conn -> {
+      conn.query("SELECT 1", testContext.asyncAssertSuccess(result -> {
+        testContext.assertNotEquals(appCtx, Vertx.currentContext());
+        conn.close();
+        async.complete();
+      }));
+    }));
+  }
+
+  @Test
+  public void testPooledConnectionInDifferentContext1(TestContext testContext) {
+    // this tests a pool is created in a non-vertx context but get connection occurs in a vertx context
+    Context appCtx = vertx.getOrCreateContext();
+    Async async = testContext.async();
+    testContext.assertNull(Vertx.currentContext());
+    PgPool pool = PgPool.pool(vertx, options, new PoolOptions());
+    appCtx.runOnContext(v1 -> {
+      pool.getConnection(testContext.asyncAssertSuccess(conn -> {
+        testContext.assertEquals(appCtx, Vertx.currentContext());
+        conn.query("SELECT 1", testContext.asyncAssertSuccess(result -> {
+          testContext.assertEquals(appCtx, Vertx.currentContext());
+          conn.close();
+          async.complete();
+        }));
+      }));
+    });
+  }
+
+  @Test
+  public void testPooledConnectionInDifferentContext2(TestContext testCtx) {
+    // this tests a pool is created in appCtx but get connection occurs in connCtx
+    Context appCtx = vertx.getOrCreateContext();
     Context connCtx = vertx.getOrCreateContext();
-    connCtx.runOnContext(v1 -> {
+    testCtx.assertNotEquals(appCtx, connCtx);
+    Async async = testCtx.async();
+    appCtx.runOnContext(v1 -> {
       PgPool pool = PgPool.pool(vertx, options, new PoolOptions());
-      appCtx.runOnContext(v -> {
+      connCtx.runOnContext(v -> {
         pool.getConnection(testCtx.asyncAssertSuccess(conn -> {
-          testCtx.assertEquals(appCtx, Vertx.currentContext());
-          conn.query("SELECT *  FROM (VALUES ('Hello world')) t1 (col1) WHERE 1 = 1", testCtx.asyncAssertSuccess(result -> {
-            testCtx.assertEquals(appCtx, Vertx.currentContext());
+          testCtx.assertEquals(connCtx, Vertx.currentContext());
+          conn.query("SELECT 1", testCtx.asyncAssertSuccess(result -> {
+            testCtx.assertEquals(connCtx, Vertx.currentContext());
             async.complete();
           }));
         }));
       });
+    });
+  }
+
+  @Test
+  public void testPooledConnectionInDifferentContext3(TestContext testCtx) {
+    // this tests a pool is created in a vertx context but get connection occurs in a non-vertx context
+    Context appCtx = vertx.getOrCreateContext();
+    Async async = testCtx.async();
+    appCtx.runOnContext(v1 -> {
+      PgPool pool = PgPool.pool(vertx, options, new PoolOptions());
+      new Thread(() -> {
+        testCtx.assertNull(Vertx.currentContext()); // non-vertx context
+        pool.getConnection(testCtx.asyncAssertSuccess(conn -> {
+          Context connCtx = Vertx.currentContext();
+          testCtx.assertNotEquals(appCtx, connCtx);
+          conn.query("SELECT 1", testCtx.asyncAssertSuccess(result -> {
+            testCtx.assertEquals(connCtx, Vertx.currentContext());
+            async.complete();
+          }));
+        }));
+      }).start();
     });
   }
 }
